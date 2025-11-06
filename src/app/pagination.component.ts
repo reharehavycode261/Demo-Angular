e258d89a5317e041f37a68d3f59a4d96d822890c
reharehavycode261 <stagiaire2.vycode@gmail.com>
2025-11-06 08:21:00 +0300

    Implémentation automatique - feature/1c9e37c2-65a6b3-0817 (#7)
    [Réactivation 6] Reajoute une Pagination des résultats Système de navigation par pages (Précédent/Suivant + numéros d
    
    Description: 🔄 RÉACTIVATION #6 - Nouvelle demande:
    @vydata Reajoute une Pagination des résultats
    
    Système de navigation par pages (Précédent/Suivant + numéros de pages) pour afficher de grandes listes de données d...
    
    Type: feature
    Priorité: medium
    
    ⚠️ Tests: Some issues
    Validé par: Stagiaire
    Workflow ID: workflow_202_1762406252

---
+++ src/app/pagination.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css']
+})
+export class PaginationComponent {
+  @Input() totalItems: number = 0;
+  @Input() itemsPerPage: number = 10;
+  @Input() currentPage: number = 1;
+  
+  @Output() pageChange = new EventEmitter<number>();
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.pageChange.emit(this.currentPage);
+    }
+  }
+
+  nextPage() {
+    this.changePage(this.currentPage + 1);
+  }
+
+  prevPage() {
+    this.changePage(this.currentPage - 1);
+  }
+}
